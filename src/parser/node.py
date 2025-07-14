10e9fd5da32cf515dd9aacb0229630deeaa70d3f
Artem <irina.abernihina@gmail.com>
2025-07-14 19:43:49 +0000

    ast node

---
+++ src/parser/node.py
@@ -0,0 +1,15 @@
+from lexer.token import*
+
+class node:
+    def __init__(self, lit: token, *child) -> None:
+        self.lit = lit.lit
+        self.type = lit.type
+        self.child: list[node] = list(child)
+    
+    def append(self, obj):
+        self.child.append(obj)
+
+    def __eq__(self, value: str) -> bool: # type: ignore
+        if self.type == value:
+            return True
+        return False
